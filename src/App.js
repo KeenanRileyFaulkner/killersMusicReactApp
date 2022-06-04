74a6fcb3b6411d61f17823d4baf59d2b4d45874f
Keenan Faulkner <keenan.r.faulkner@gmail.com>
2022-06-04 00:06:51 -0600

    Initial react app setup with tailwind config for styling

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavBar from './components/NavBar';
+import Content from './components/Content';
+
+function App() {
+  return (
+    <div className='page-container'>
+      <NavBar />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
