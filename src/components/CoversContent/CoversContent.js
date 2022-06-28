e5712888ef8a5b5b74226096aae35eee2fde64e7
Keenan Faulkner <keenan.r.faulkner@gmail.com>
2022-06-27 18:27:37 -0600

    Added nametag below cover link

--- src/components/CoversContent/CoversContent.js
+++ src/components/CoversContent/CoversContent.js
@@ -43,25 +43,55 @@ const CoversContent = ({ covers=coversArr }) => {
         <div className='contentContainer bg-itm-band-photo bg-[center_10px] centerItems'>
             <AudioPlayer src={audioControls.src} handleEnded={stopAllPlaying} />
             {coverList.map((cover, index) => {
-                return <CoverLink color={cover.color} image={cover.image} onClick={() => handleClick(index)} musicPlaying={cover.playing} key={cover.name} />
+                return <CoverLink color={cover.color} image={cover.image} onClick={() => handleClick(index)} musicPlaying={cover.playing} coverName={cover.name} key={cover.name} />
             })}
         </div>
     )
 }
 
 
-const CoverLink = ({ color='bg-gray-800', image='bg-logo', onClick, musicPlaying }) => {
+const CoverLink = ({ color='bg-gray-800', image='bg-logo', onClick, musicPlaying, coverName }) => {
+
+    let borderColor;
+    switch (color) { //switch statement due to tailwind bug; color palette should be available to border utility class but is not
+        case 'bg-steel-blue':
+            borderColor = 'border-[#538b90]';
+            break;
+        case 'bg-vegas-yellow':
+            borderColor = 'border-[#d8b666]';
+            break;
+        case 'bg-mocha-brown':
+            borderColor = 'border-[#382417]';
+            break;
+        case 'bg-bb-black':
+            borderColor = 'border-[#1f1c23]';
+            break;
+        case 'bg-pearly-white':
+            borderColor = 'border-[#f8fbfb]';
+            break;
+        case 'bg-amber-wave':
+            borderColor = 'border-[#9f5c0c]';
+            break;
+        default:
+            borderColor = 'border-white';
+    }
 
     return (
-        <div className={`${image} ${color} albumLink`}>
-            <button onClick={onClick}>
-                {musicPlaying ? (
-                    <MdPauseCircleFilled size='58' className='audio-icon' />
-                ) : (
-                    <FaPlayCircle size='50' className='audio-icon' />
-                )}
-            </button>
+        <div>
+            <div className={`${image} ${color} albumLink mx-8 mt-4`}>
+                <button onClick={onClick}>
+                    {musicPlaying ? (
+                        <MdPauseCircleFilled size='58' className='audio-icon' />
+                    ) : (
+                        <FaPlayCircle size='50' className='audio-icon' />
+                    )}
+                </button>
+            </div>
+            <div className={`cover-name ${borderColor}`}>
+                {coverName}
+            </div>
         </div>
+        
     );
 }
 
